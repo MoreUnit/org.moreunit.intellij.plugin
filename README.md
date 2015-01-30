3fcc43a6ecc7583633cee3b27848ca547e38280d
Nicolas Demengel <nicolas.demengel@gmail.com>
2015-01-30 08:50:43 +0100

    Set-up a Maven-compatible plugin

---
+++ README.md
@@ -0,0 +1,9 @@
+# MoreUnit For Intellij
+
+## Development
+### Getting Started
+
+1. Download [sources of IntelliJ Community Edition](#TODO-link) and unpack them into ../idea-ce
+2. `./build.sh` (will download IntelliJ CE as a Maven dependency, test the code and package the plugin)
+3. Open the project within IntelliJ.
+
---
+++ src/test/java/org/moreunit/intellij/plugin/JumpActionTest.java
@@ -0,0 +1,14 @@
+package org.moreunit.intellij.plugin;
+
+import com.intellij.testFramework.LightPlatformCodeInsightTestCase;
+
+public class JumpActionTest extends LightPlatformCodeInsightTestCase {
+    @Override
+    protected String getTestDataPath() {
+        return "../../org.moreunit.intellij.plugin/src/test/resources";
+    }
+
+    public void testSomething() {
+        // TODO test something
+    }
+}
