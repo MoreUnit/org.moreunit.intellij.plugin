cf9a39bc97ff0b01d75e1e0541b7da9e47983ec6
Nicolas Demengel <nicolas.demengel@gmail.com>
2015-01-31 22:03:52 +0100

    OK, Travis caching is not enabled for public repos

--- download-idea-sources.sh
+++ download-idea-sources.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# attempt at understanding why Travis caching doesn't work
-echo "travis env var: $TRAVIS_BUILD_DIR/../idea-ce"
-echo "pwd: $(pwd)/../idea-ce"
-
 if [ ! -d ../idea-ce ]; then
   # the repo is huge, let's only get its last state
   git clone --depth 1 git://git.jetbrains.org/idea/community.git ../idea-ce
