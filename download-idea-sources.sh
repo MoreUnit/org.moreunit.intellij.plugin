08ca1647fc76a0ce263dbb40c40af7c48812e07f
Nicolas Demengel <nicolas.demengel@gmail.com>
2015-01-31 22:00:54 +0100

    Reduce DL size of Idea sources. Debug Travis caching

--- download-idea-sources.sh
+++ download-idea-sources.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# attempt at understanding why Travis caching doesn't work
+echo "travis env var: $TRAVIS_BUILD_DIR/../idea-ce"
+echo "pwd: $(pwd)/../idea-ce"
+
 if [ ! -d ../idea-ce ]; then
-  git clone git://git.jetbrains.org/idea/community.git ../idea-ce
+  # the repo is huge, let's only get its last state
+  git clone --depth 1 git://git.jetbrains.org/idea/community.git ../idea-ce
 fi
+
